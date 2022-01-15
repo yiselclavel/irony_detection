0210ecb619d49b63bd8c6c6eeabf515956e5bbba
Yisel Clavel Quintero <yclavelq@uho.edu.cu>
2022-01-15 04:47:45 -0500

    Update LSTM-Glove6B.py

--- LSTM-Glove6B.py
+++ LSTM-Glove6B.py
@@ -3,32 +3,29 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import recall_score, precision_score, f1_score, balanced_accuracy_score
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Embedding
+from tensorflow.python.keras.layers import Dense, Embedding, Dropout
+from tensorflow_core.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow_core.python.keras.layers import LSTM, Flatten
-import keras.backend.tensorflow_backend as KTF
+import matplotlib.pyplot as plt
+from tensorflow_core.python.keras.saving import load_model
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth=True
-session = tf.compat.v1.Session(config=config)
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# config = tf.compat.v1.ConfigProto()
+# config.gpu_options.allow_growth=True
+# session = tf.compat.v1.Session(config=config)
 
 class IronyClassifier:
-    # seed = 7346
-    # np.random.seed(seed)
-    EMBEDDING_DIM = 100
-    dataframe = pd.DataFrame()
-    dataframe = pd.read_csv('./Semeval.csv', encoding='ISO-8859-1')
-
+    # read the data
+    dataframe = pd.read_csv('semeval train.csv', encoding='ISO-8859-1', sep=';;', engine='python')
     cols = ['text', 'label']
     dataframe.columns = cols
     X = dataframe.loc[:, 'text']
     Y = dataframe.loc[:, 'label']
     print(dataframe.shape)
-    # X = X.reshape((len(X),1))
     print(X.count())
     print(Y.count())
     Y = np.asarray(Y)
@@ -48,12 +45,12 @@ class IronyClassifier:
     # define Tokenizer
     MAX_SEQUENCE_LENGTH = max([len(s.split()) for s in X])
     tokenizer_obj = Tokenizer(num_words=len(words))
-    # tokenizer_obj = Tokenizer(filters='\t\n')
     tokenizer_obj.fit_on_texts(words)
     word_index = tokenizer_obj.word_index
-    vocab_size = len(word_index) + 1  # define vocabulary size
+    vocab_size = len(word_index) + 1  # vocabulary size
 
     # construct embedding matrix
+    EMBEDDING_DIM = 100
     embeddings_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
     for word, index in word_index.items():
         if index > vocab_size - 1:
@@ -72,46 +69,59 @@ class IronyClassifier:
     print(X_pad.shape, Y.shape)
 
     # define 10-fold cross validation
-    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+    kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
     count = 0
     accuracy_list = list()
     precision_list = list()
     recall_list = list()
     f1_list = list()
+    fig, ax = plt.subplots(figsize=(14, 8))
 
     for train, test in kfold.split(X_pad, Y):
         count += 1
         print('Fold: {0}'.format(count))
-        print(X_pad[train].shape, Y[train].shape)
         # create model
         model = Sequential()
         model.add(Embedding(input_dim=vocab_size, output_dim=EMBEDDING_DIM,
                             input_length=MAX_SEQUENCE_LENGTH, weights=[embeddings_matrix], trainable=True))
-        model.add(LSTM(8, return_sequences=False, return_state=False, stateful=False, dropout=0.5, activation='sigmoid'))
+        model.add(LSTM(32, return_sequences=False, dropout=0.2))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dropout(rate=0.2))
         model.add(Flatten())
         model.add(Dense(1, activation='sigmoid'))
         # compile model
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         # train the model
         print('Train...')
-        model.fit(X_pad[train], Y[train], batch_size=128, epochs=25, verbose=1)
-        print('')
+        es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=10)
+        mc = ModelCheckpoint('best_model' + str(count) + '.h5', monitor='val_accuracy', mode='max', verbose=1,
+                             save_best_only=True)
+        history = model.fit(X_pad[train], Y[train], validation_split=0.3, batch_size=64, epochs=20, verbose=2, callbacks=[es, mc])
+        saved_model = load_model('best_model' + str(count) + '.h5')
+        print()
+
         # evaluate the model
-        y_predict = model.predict_classes(X_pad[test])
-        b_acc = balanced_accuracy_score(Y[test], y_predict)
+        y_predict = saved_model.predict_classes(X_pad[test])
+        acc = accuracy_score(Y[test], y_predict)
         recall = recall_score(Y[test], y_predict, average='weighted')
         precision = precision_score(Y[test], y_predict, average='weighted')
         f1 = f1_score(Y[test], y_predict, average='weighted')
-        accuracy_list.append(b_acc)
-        precision_list.append(precision)
-        recall_list.append(recall)
-        f1_list.append(f1)
-        print('Test b_accuracy:', b_acc)
+        print('Test accuracy:', acc)
         print('Test precision:', precision)
         print('Test recall:', recall)
         print('Test f1:', f1)
+        accuracy_list.append(acc)
+        precision_list.append(precision)
+        recall_list.append(recall)
+        f1_list.append(f1)
+        print(history.history.keys())
+        plt.plot(history.history['accuracy'], color='blue')
+        plt.plot(history.history['val_accuracy'], color='red')
+        plt.title(str(count))
+        plt.legend({"validation accuracy", "training accuracy"})
+        plt.show()
 
-    print('avg b_accuracy {0}'.format(np.average(accuracy_list)))
+    print('avg accuracy {0}'.format(np.average(accuracy_list)))
     print('avg precision {0}'.format(np.average(precision_list)))
     print('avg recall {0}'.format(np.average(recall_list)))
     print('avg f1 {0}'.format(np.average(f1_list)))
